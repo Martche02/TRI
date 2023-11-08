203f756089b152207bb56277dddf94cd4d078eaa
Martche02 <marcelo.gonda.stangler@gmail.com>
2023-11-07 22:53:57 -0300

    git to heroku

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask, request, jsonify, render_template, redirect, url_for
 import pandas as pd
 import json
-from pyirt import irt
+from girth import *
+import numpy as np
 
 app = Flask(__name__)
-
-# Dados globais para armazenar o gabarito e as respostas dos alunos
-answer_key = {}
-item_params = {}
 
 @app.route('/')
 def index():
@@ -17,9 +14,13 @@ def index():
 def professor():
     return render_template('professor.html')
 
+# Nova função aluno() modificada
 @app.route('/aluno')
 def aluno():
-    return render_template('aluno.html')
+    with open('simulated_bd.json', 'r') as file:
+        exams = json.load(file)
+    exam_names = [exam['Nome do Exame'] for exam in exams]
+    return render_template('aluno.html', exam_names=exam_names) 
 
 @app.route('/upload_csv', methods=['POST'])
 def upload_csv():
@@ -31,11 +32,20 @@ def upload_csv():
         return 'Nenhum arquivo selecionado', 400
 
     if file and file.filename.endswith('.csv'):
-        df = pd.read_csv(file)
-        global answer_key
-        answer_key = json.loads(request.form['answers'])
-        global item_params
-        item_params = perform_calibration(df, answer_key)
+        sb = pd.read_csv(file) # aqui você deve importar o csv response
+        dT = np.array([i.split(",") for i in sb.split("\n")])
+        d = dT.T
+        objt = [[ 0 if dT[i][j]=="0" else 1 for i in range(len(d[j])) if i != 0] for j in range(len(d)) if len(set(d[j])) == 3]
+        data = np.array(objt)
+        gTRI = twopl_jml(data) # salvar em formato json, isto é o compulado de parametros do exame. Deve ser importado do json quando o aluno requisitar a nota
+        gTRI['Exame'] = str(json.loads(request.form['Nome do Exame']))
+        with open("bd.json") as fp:
+          bd = json.load(fp)
+        bd.update(gTRI)
+        with open("bd.json", 'w') as json_file:
+            json.dump(bd, json_file, 
+                        indent=4,  
+                        separators=(',',': '))
         
         return 'CSV e Gabarito processados com sucesso', 200
     else:
@@ -43,32 +53,12 @@ def upload_csv():
 
 @app.route('/calculate_score', methods=['POST'])
 def calculate_score():
-    student_answers = request.json['answers']
-    score = calculate_student_score(student_answers, item_params, answer_key)
+    prova_do_aluno = np.array(request.json['answers'])
+    with open("bd.json") as fp:
+          bd = json.load(fp)
+    gTRI = bd[request.json['Nome do Exame']] ## unico problema
+    score = ability_eap(prova_do_aluno, gTRI["Difficulty"], gTRI["Discrimination"])*100+500 # retorna nota do aluno
     return jsonify({'score': score})
-
-def perform_calibration(df, answer_key):
-    src_data = []
-    for idx, row in df.iterrows():
-        user_id = str(row['user_id'])  # Ou o identificador de cada aluno
-        for q in range(1, 26):  # Supondo 25 questões
-            item_id = str(q)
-            is_correct = 1 if row[f'Q{q}'] == answer_key[f'answer{q}'] else 0
-            src_data.append((user_id, item_id, is_correct))
-    
-    _, item_param_dict, _ = irt(src_data, est_theta=False)
-    return item_param_dict
-
-def calculate_student_score(answers, item_params, answer_key):
-    student_data = []
-    for q in range(1, 26):  # Supondo 25 questões
-        item_id = str(q)
-        is_correct = 1 if answers[f'question{q}'] == answer_key[f'answer{q}'] else 0
-        student_data.append((None, item_id, is_correct))
-    
-    _, _, person_param_dict = irt(student_data, est_item=False, item_param=item_params)
-    theta = next(iter(person_param_dict.values()))[0]
-    return theta
 
 if __name__ == '__main__':
     app.run(debug=True)
